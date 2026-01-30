ecd2b38c29ccb9e18ed0fd52801b27e902c2b8f7
Micaela Asselborn <asselborn96@gmail.com>
2026-01-30 16:24:25 -0300

    Agrega MVC de autenticación

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import cors from 'cors';
 import express from 'express';
 import path from 'path';
 import 'dotenv/config';
-
 import { connectDB } from './config/database';
 
 const app = express();
@@ -11,16 +10,24 @@ const PORT = process.env.PORT || 8000;
 // Middleware para interpretar JSON
 app.use(express.json());
 
-// Habilita todas las peticiones cross-origin
+// Habilita todas las peticiones cross-origin (Conectar diferentes puertos)
 app.use(cors());  
 
 // Middleware para servir archivos estáticos desde la carpeta "public"
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// Api de prueba
-app.get('/api/salud', (req, res) => {
-    res.json({status: 'OK', message: 'Api funcionando'})
+// Api visible para todo el mundo
+app.get('/public', (req, res) => {
+  res.json({
+    message: '¡Bienvenido a la veterinaria "Patitas Felices"!'
+  });
 });
+
+// Api administradores
+app.get('/admin', (req, res) => {
+  res.json({status: 'OK', message: 'Solo el administrador de la veterinaria puede entrar aca.'})
+});
+
 
 // Conectar a MongoDB y luego iniciar el servidor HTTP
 connectDB()
